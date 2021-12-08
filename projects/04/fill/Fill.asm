38e9d3382b6b2ecc72f7c188d400164b27e697c1
xianghao <xianghaowang2001@outlook.com>
2021-12-08 14:58:51 +0800

    Add Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,69 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// 0: white; -1: black
+@COLOR
+M=0
+
+// the screen size
+@8192
+D=A
+@n
+M=D
+
+(K_LISTEN)
+    @KBD
+    D=M
+
+    // choose color
+    @BLACK
+    D;JNE
+    @COLOR
+    M=0
+    @INIT_DRAW
+    0;JMP
+    (BLACK)
+        @COLOR
+        M=-1
+
+
+    (INIT_DRAW)
+    @i
+    M=0
+    @SCREEN
+    D=A
+    @ADDR
+    M=D
+
+    (LOOP)
+        @i
+        D=M
+        @n
+        D=M-D
+        @END_LOOP
+        D;JEQ
+
+        // continue to draw
+        @COLOR
+        D=M
+        @ADDR
+        A=M
+        M=D
+
+        @ADDR
+        M=M+1
+        @i
+        M=M+1
+
+        @LOOP
+        0;JMP
+
+    (END_LOOP)
+
+    @K_LISTEN
+    0;JMP
+
+
+    
+
+
